e34b37e6f24f23586525ac2be6d271f6257e61de
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-18 15:59:26 -0500

    tests work

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,4 +1,6 @@
 // import our actions
+import { reducer } from '../utils/reducers';
+
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -21,3 +23,24 @@ test('UPDATE_PRODUCTS', () => {
   expect(newState.products.length).toBe(2);
   expect(initialState.products.length).toBe(0);
 });
+
+test('UPDATE_CATEGORIES', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}]
+  });
+
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
+
+test('UPDATE_CURRENT_CATEGORY', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: '2'
+  });
+
+  expect(newState.currentCategory).toBe('2');
+  expect(initialState.currentCategory).toBe('1');
+});
+
