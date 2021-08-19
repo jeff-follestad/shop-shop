013b5c0bb7c77144b8dbc27128f7416ed5369766
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-18 19:01:59 -0500

    tests for carts

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,9 +1,16 @@
 import { useReducer } from 'react';
+
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
-} from "./actions";
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
+} from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
